d372699ca309451048e1daf9472254d57320aa8e
identityofsine <identityofsine@gmail.com>
2025-05-26 16:16:34 -0400

    added build version log

--- api/dto/buildinfo/buildinfo.go
+++ api/dto/buildinfo/buildinfo.go
@@ -1,4 +1,4 @@
-package storedlogs
+package buildinfo
 
 import (
 	"time"
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	. "github.com/identityofsine/fofx-go-gin-api-template/cmd/router"
 	. "github.com/identityofsine/fofx-go-gin-api-template/pkg/buildinfo/service"
+	buildInfoService "github.com/identityofsine/fofx-go-gin-api-template/pkg/buildinfo/service"
 	cronjobs "github.com/identityofsine/fofx-go-gin-api-template/pkg/cron/jobs"
 	cron "github.com/identityofsine/fofx-go-gin-api-template/pkg/cron/services"
 	"github.com/identityofsine/fofx-go-gin-api-template/pkg/db"
@@ -30,11 +32,17 @@ func main() {
 		log.Fatalf("Error running migrations: %v", derr.Error())
 	}
 
+	// Get Build Info
+	buildInfo, err := buildInfoService.GetBuildInfo()
+	if err != nil {
+		log.Fatalf("Error getting build info: %v", err)
+	}
+
 	if _, err := GetBuildInfo(); err != nil {
 		storedlogs.LogFatal("Error getting build info: %v", err)
 	}
 
-	storedlogs.LogInfo("Starting application")
+	storedlogs.LogInfo(fmt.Sprintf("Starting application under build version %s:<%s> built on: %s\n", buildInfo.Version, buildInfo.CommitHash, buildInfo.BuildDate))
 
 	cron.AddJob(cronjobs.GetAuthTokenDeleteJob())
 
