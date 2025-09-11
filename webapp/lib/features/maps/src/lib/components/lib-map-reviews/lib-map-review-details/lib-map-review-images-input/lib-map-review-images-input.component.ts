70b5b48eedd729aab2ebde457accc66b74d72c3f
identityofsine <identityofsine@gmail.com>
2025-09-10 22:51:10 -0400

    adding images

--- webapp/lib/features/maps/src/lib/components/lib-map-reviews/lib-map-review-details/lib-map-review-images-input/lib-map-review-images-input.component.ts
+++ webapp/lib/features/maps/src/lib/components/lib-map-reviews/lib-map-review-details/lib-map-review-images-input/lib-map-review-images-input.component.ts
@@ -1,13 +1,27 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { MapReviewImagesInputThumbnail } from './components/lib-map-review-images-input-image-thumbnail.component';
+import { AddButtonComponent } from '../../../lib-add-button/lib-add-button.component';
+import { MapImage } from '@arch-shared/types';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'lib-map-review-images-input',
   templateUrl: './lib-map-review-images-input.component.html',
   styleUrls: ['./lib-map-review-images-input.component.scss'],
-  imports: [MapReviewImagesInputThumbnail],
+  imports: [MapReviewImagesInputThumbnail, AddButtonComponent],
 })
 export class MapReviewImagesInputComponent {
+  readonly images = input<MapImage[]>([]);
 
+
+  public openAddImage() {
+    const view = this.containerRef.createComponent(MapImageDetailsComponent);
+    //view.setInput();
+    view.instance.shouldReloadImage.subscribe((res) => {
+      if (res) {
+        this.reloadImages();
+      }
+      view.destroy();
+    });
+  }
 }
